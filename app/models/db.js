56baa2418596139631f2a840d7ba7f7781b9bc11
Vishal Ranjan <vishivish18@gmail.com>
2016-10-17 23:46:04 +0530

    setting db on mongolab

--- app/models/db.js
+++ app/models/db.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tweetilytics1',function(){
-	console.log('mongodb connected');
+var config = require('../../config');
+mongoose.connect(config.mongoURL, function() {
+    console.log('mongodb connected');
 })
+mongoose.connection.on('open', function(ref) {
+    console.log('Connected to Mongo server...');
+});
 
 module.exports = mongoose;
