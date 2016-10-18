df94236f5983d01e884030707084b52af1c32a90
Vishal Ranjan <vishivish18@gmail.com>
2016-10-18 21:54:23 +0530

    pushing to heroku with config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key: 'ESgzekaIdnLTGeSnCqtj4YBfJ',
+    consumer_secret: 'ENMmGeEN1OUmXUOXxUYkLDoaF3JFSizoePX1nvezgx34FMprKr',
+    access_token: '65933955-MbSxnh0ULiTC03nzsPw33RWpf7f5G9Koex8LAfKRB',
+    access_token_secret: 'eefxFosWd76mcDRgeO7scdu1Ryrbi1L79uPoB9e5P18DZ',
+    mongoURL: 'mongodb://admin:admin@ds059496.mlab.com:59496/heroku_vd33zv3x'
+}
